3fc51d9cc24b69cb39b401be2423182ebc57db05
Carlos <cmb1197@hotmail.com>
2021-07-31 09:26:35 -0500

    a

---
+++ src/main/java/com/apixmlbuilder/xmlbuilderspring/XmlbuilderspringApplication.java
@@ -0,0 +1,13 @@
+package com.apixmlbuilder.xmlbuilderspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlbuilderspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlbuilderspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apixmlbuilder/xmlbuilderspring/XmlbuilderspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apixmlbuilder.xmlbuilderspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlbuilderspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
