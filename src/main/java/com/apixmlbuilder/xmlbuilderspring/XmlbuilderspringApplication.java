ce818b62e6a8fd9bc2c9e666486d5a3978acf11e
Carlos <cmb1197@hotmail.com>
2021-07-31 09:42:56 -0500

    creacion pack CORRE

--- src/main/java/com/apixmlbuilder/xmlbuilderspring/XmlbuilderspringApplication.java
+++ src/main/java/com/apixmlbuilder/xmlbuilderspring/XmlbuilderspringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XmlbuilderspringApplication {
 
 	public static void main(String[] args) {
+            
 		SpringApplication.run(XmlbuilderspringApplication.class, args);
 	}
 
